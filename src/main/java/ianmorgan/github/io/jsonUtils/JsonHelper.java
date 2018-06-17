555661833d9c9206f56371f7c5ba9cf0295f6f72
Ian Morgan <ian.j.morgan@gmail.com>
2018-06-17 10:25:13 +0100

    added support for passing Strings directly to the helper

--- src/main/java/ianmorgan/github/io/jsonUtils/JsonHelper.java
+++ src/main/java/ianmorgan/github/io/jsonUtils/JsonHelper.java
@@ -11,6 +11,18 @@ import java.util.Map;
 public class JsonHelper {
 
     /**
+     * Take a Json string and convert to normal Java Map by
+     * examining each key in the corresponding JSONObject
+     *
+     * @param json The json string
+     * @return a Java Map with same content as the original JSON
+     */
+
+    public static Map<String,Object> jsonToMap(String json){
+        return jsonToMap(new JSONObject(json));
+    }
+
+    /**
      * Take a JSONObject and convert to normal Java Map by
      * examining each key.
      *
@@ -33,6 +45,16 @@ public class JsonHelper {
         return result;
     }
 
+    /**
+     * Take a String holding an array an convert to a normal Java List , examining each
+     * element in the corresponding JSONArray individually
+     *
+     * @param jsonArray The string holding an array in JSON syntax
+     * @return A Java List with the same content as the original array
+     */
+    public static List<Object> jsonToList(String jsonArray) {
+        return jsonToList(new JSONArray(jsonArray));
+    }
 
     /**
      * Take a JSONArray an convert to a normal Java List , examining each
